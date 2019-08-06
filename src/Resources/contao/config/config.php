41221302f6953e66cb1364bb90a0d41663d2f4d4
vstuber <stuber@leadingsystems.de>
2019-08-06 17:55:29 +0200

    Fix namespace

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,5 +2,5 @@
 
 namespace LeadingSystems\Lscss4c;
 
-$GLOBALS['TL_HOOKS']['getPageLayout'][] = array('LeadingSystems\Lsjs4c\lscss4C_controller', 'getLayoutSettingsForGlobalUse');
-$GLOBALS['TL_HOOKS']['generatePage'][] = array('LeadingSystems\Lsjs4c\lscss4C_controller', 'insertLsjs');
+$GLOBALS['TL_HOOKS']['getPageLayout'][] = array('LeadingSystems\Lscss4c\lscss4C_controller', 'getLayoutSettingsForGlobalUse');
+$GLOBALS['TL_HOOKS']['generatePage'][] = array('LeadingSystems\Lscss4c\lscss4C_controller', 'insertLsjs');
