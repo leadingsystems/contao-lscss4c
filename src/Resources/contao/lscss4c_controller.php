d518d4fa3012e74ed30a565af2034c4333f32120
vstuber <stuber@leadingsystems.de>
2019-08-06 20:37:43 +0200

    Finish basic functionality

--- src/Resources/contao/lscss4c_controller.php
+++ src/Resources/contao/lscss4c_controller.php
@@ -4,9 +4,9 @@ namespace LeadingSystems\Lscss4c;
 use function LeadingSystems\Helpers\ls_getFilePathFromVariableSources;
 
 class lscss4C_controller extends \Controller {
-	protected $str_folderUpPrefix = '_dup4_/';
+    protected static $objInstance;
 
-	protected static $objInstance;
+    private $str_pathToOutputFile = 'assets/css/lscss4c.css';
 
 	protected function __construct() {
 		parent::__construct();
@@ -23,13 +23,34 @@ class lscss4C_controller extends \Controller {
 		return self::$objInstance;
 	}
 
-	public function insertLsjs() {
+	public function insertLscss() {
 		if (!$GLOBALS['lscss4c_globals']['lscss4c_lessFileToLoad']) {
 			return;
 		}
 
-		die('FIXME: ' . __METHOD__);
-	}
+        if (
+            !file_exists(TL_ROOT . '/' . $this->str_pathToOutputFile)
+            || $GLOBALS['lscss4c_globals']['lscss4c_noCache']
+        ) {
+            $str_filePath = $GLOBALS['lscss4c_globals']['lscss4c_lessFileToLoad'];
+            $str_dirPath = \dirname($str_filePath);
+
+            $arr_options = array
+            (
+                'strictMath' => true,
+                'import_dirs' => array(TL_ROOT . '/' . $str_dirPath => $str_dirPath),
+                'sourceMap' => $GLOBALS['lscss4c_globals']['lscss4c_debugMode'],
+                'compress' => !$GLOBALS['lscss4c_globals']['lscss4c_noMinifier']
+            );
+
+            $obj_parser = new \Less_Parser();
+            $obj_parser->SetOptions($arr_options);
+            $obj_parser->parseFile(TL_ROOT . '/' . $str_filePath);
+            file_put_contents(TL_ROOT . '/' . $this->str_pathToOutputFile, $obj_parser->getCss());
+        }
+
+        $GLOBALS['TL_CSS'][] = $this->str_pathToOutputFile;
+    }
 
 	public function getLayoutSettingsForGlobalUse(\PageModel $objPage, \LayoutModel $objLayout, \PageRegular $objPageRegular) {
 		$GLOBALS['lscss4c_globals']['lscss4c_lessFileToLoad'] = ls_getFilePathFromVariableSources($objLayout->lscss4c_lessFileToLoad);
