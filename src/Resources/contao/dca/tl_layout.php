6e347657f355331a331ec6e4cbe1412816be3583
tbu123 <buchmann@leadingsystems.de>
2023-09-18 11:03:10 +0200

    changed tl_layout.php; weitere Anlagetests, WIP

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -3,8 +3,9 @@
 namespace LeadingSystems\Lscss4c;
 
 
-
+/*
 $GLOBALS['TL_DCA']['tl_layout'] = array(
+
     'config' => array(
         'dataContainer' => DC_Table::class,
 
@@ -17,25 +18,53 @@ $GLOBALS['TL_DCA']['tl_layout'] = array(
         )
 
     ),
+
+
     'fields' =>  array(
+
         'id' => array (
             'sql'                     => "int(10) unsigned NOT NULL"
         ),
 
+
         'lscss4c_scssFileToLoad' => array (
-            'sql'                     => "blob NULL"
+            'sql'                     => "blob NULL",
+
+            'label' => &$GLOBALS['TL_LANG']['tl_layout']['lscss4c_scssFileToLoad'],
+            'exclude' => true,
+            'inputType' => 'fileTree',
+            'eval' => array(
+                'multiple' => false,
+                'tl_class'=>'clr',
+                'files' => true,
+                'filesOnly' => true,
+                'fieldType' => 'radio',
+                'extensions'=>'scss'
+            )
         ),
 
         'lscss4c_debugMode' => array (
-            'sql'                     => "char(1) NOT NULL default ''"
+            'sql'                     => "char(1) NOT NULL default ''",
+            'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lscss4c_debugMode'],
+            'exclude'                 => true,
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'m12')
         ),
 
         'lscss4c_noCache' => array (
-            'sql'                     => "char(1) NOT NULL default ''"
+            'sql'                     => "char(1) NOT NULL default ''",
+            'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lscss4c_noCache'],
+            'exclude'                 => true,
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'m12')
         ),
 
         'lscss4c_noMinifier' => array (
-            'sql'                     => "char(1) NOT NULL default ''"
+            'sql'                     => "char(1) NOT NULL default ''",
+            'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lscss4c_noMinifier'],
+            'exclude'                 => true,
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'m12')
         ),
 
         'lscss4c_pathsToConsiderForHash' => array (
@@ -43,16 +72,21 @@ $GLOBALS['TL_DCA']['tl_layout'] = array(
         ),
 
         'lscss4c_cacheHash' => array (
-            'sql'                     => "varchar(255) NOT NULL default ''"
+            'sql'                     => "varchar(255) NOT NULL default ''",
+            'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lscss4c_pathsToConsiderForHash'],
+            'exclude'                 => true,
+            'inputType'               => 'textarea',
+            'eval'                    => array('tl_class'=>'clr')
         )
 
     ),
 
     'palettes' => array(
-		'default' => '{title_legend},title,alias;formId;'
-	),
+		'default' => '{lscss4c_legend},lscss4c_scssFileToLoad,lscss4c_debugMode,lscss4c_noCache,lscss4c_noMinifier,lscss4c_pathsToConsiderForHash'
+	)
 
 );
+*/
 
 /*
 $GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ';{lscss4c_legend},lscss4c_scssFileToLoad,lscss4c_debugMode,lscss4c_noCache,lscss4c_noMinifier,lscss4c_pathsToConsiderForHash';
