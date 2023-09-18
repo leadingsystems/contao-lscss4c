6ea9ac42389cc0e15e5af257b4a4ed939a2d0212
tbu123 <buchmann@leadingsystems.de>
2023-09-18 08:25:31 +0200

    changed tl_layout.php; remove database.sql, WIP

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -2,6 +2,59 @@
 
 namespace LeadingSystems\Lscss4c;
 
+
+
+$GLOBALS['TL_DCA']['tl_layout'] = array(
+    'config' => array(
+        'dataContainer' => DC_Table::class,
+
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' => 'primary'
+            )
+        )
+
+    ),
+    'fields' =>  array(
+        'id' => array (
+            'sql'                     => "int(10) unsigned NOT NULL"
+        ),
+
+        'lscss4c_scssFileToLoad' => array (
+            'sql'                     => "blob NULL"
+        ),
+
+        'lscss4c_debugMode' => array (
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+
+        'lscss4c_noCache' => array (
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+
+        'lscss4c_noMinifier' => array (
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+
+        'lscss4c_pathsToConsiderForHash' => array (
+            'sql'                     => "text NULL"
+        ),
+
+        'lscss4c_cacheHash' => array (
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        )
+
+    ),
+
+    'palettes' => array(
+		'default' => '{title_legend},title,alias;formId;'
+	),
+
+);
+
+/*
 $GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ';{lscss4c_legend},lscss4c_scssFileToLoad,lscss4c_debugMode,lscss4c_noCache,lscss4c_noMinifier,lscss4c_pathsToConsiderForHash';
 
 $GLOBALS['TL_DCA']['tl_layout']['fields']['lscss4c_scssFileToLoad'] = array(
@@ -45,3 +98,4 @@ $GLOBALS['TL_DCA']['tl_layout']['fields']['lscss4c_pathsToConsiderForHash'] = ar
 	'inputType'               => 'textarea',
 	'eval'                    => array('tl_class'=>'clr')
 );
+*/
